3f356dc0fc17406e1fd0578aab2c67e392395d45
Shavej-Malik <shavejsbi@gmail.com>
2026-08-02 10:07:21 +0530

    Js Beginning

---
+++ JS Beginning/script.js
@@ -0,0 +1,19 @@
+// var let cont 
+var a= 20
+a=9 //true no error
+let b = 30
+b=7 //true No error
+const c = 40 
+c=4 //false Error (we cant assign again of const value with same variable)
+
+console.log("Hello friends")// print anything into console
+console.error("Error")// to show error on the console
+console.warn("Wrning")//to giving warning 
+prompt("Name ?")// take input 
+alert('Hacked!')// do alert
+slice(2,5)// give a part of any string(starting fom 2 to 5-1(4))
+split(' ')//can split any string with (' ')
+.join()// joined all string afte split
+replace('a','b')//replace only one a from b
+replaceAll('a','b')//replace all a from b whatever presnt a in string
+includes("a")//give true if a present   
