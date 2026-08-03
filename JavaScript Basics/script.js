34a01d1e89fde1f23daa5cf846d686f1cc39bbaa
Shavej-Malik <shavejsbi@gmail.com>
2026-08-03 09:01:36 +0530

    JS Basics

---
+++ JavaScript Basics/script.js
@@ -0,0 +1,25 @@
+var a = 12 //we were creating variable in old JS
+// example :-
+{
+    var a = 45
+}
+//console.log(a)// we cannot use value of a in outside bracket. we can use with the help of var
+
+let b = 90//Now in new JA we are now creating like this. use this maximum time in JS 
+// example :-
+{
+    let c = 45
+}
+//console.log(c)// we cannot use value of a, in outside bracket. but with let is possible. Error ill produce here
+
+const d = 10 //use when we cannot change this value again
+
+//console.info("Malik")
+//console.table({name:'Malik'})//use object
+
+//let name=prompt("Name?")//using for getting input
+//console.log(name)
+
+//template string wruite in backtick (``)
+let val = `Shavej Malik ${2+2}`
+console.log(val)
