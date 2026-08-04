9a53a0ff56de310ad30360d9e646418fed8f27ee
Shavej-Malik <shavejsbi@gmail.com>
2026-08-04 10:38:57 +0530

    Basic Problem

---
+++ Basic Problem in JS/script.js
@@ -0,0 +1,28 @@
+// var a=prompt("Name?");
+// alert(`hey${a}`);
+
+// var a= 12;
+// let b= "Malik"
+// var c= true
+// var d= NaN //Not a Number
+// console.log(typeof cd)
+
+// let age= prompt("Enter Number")// always retun string
+// age= Number(age)
+// console.log(typeof age)
+
+// let msg ='I love Myself'
+// console.log(msg.slice(2,6))
+
+// let msg='Hello World'
+// console.log(msg.split(''))
+
+// let msg='I love'
+// console.log(msg.replace('love','study'))
+
+// let name= prompt("Enter Your Name?")
+// console.log(`Hey ${name} Welcome to India`)
+
+// let st= 'Hello. I love world'
+// console.log(st.includes('love'))
+
