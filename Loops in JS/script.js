a0cec7ab1559e65f3543cf2939ccc305c68601ce
Shavej-Malik <shavejsbi@gmail.com>
2026-08-07 08:48:18 +0530

    Loops in JS

---
+++ Loops in JS/script.js
@@ -0,0 +1,44 @@
+//if else 
+    // if(12){
+    //     console.log("Hello world")
+    // }else{
+    //     console.log("Bye")
+    // }
+//Truthy and Falsy
+// - 0 " " false NaN null undefined document.all (All are show false)
+// - instead of these all are True
+//else-if
+    // if(12>13){
+    //     console.log("Hello")
+    // }
+    // else if(13>14){
+    //     console.log("Hello2")
+    // }
+    // else if(14>15){
+    //     console.log("Hello3")
+    // }
+    // else{
+    //     console.log("End")
+    // }
+//Ternary Operator -> Condition ? True case : false case
+    // 12>13 ? console.log("True") : console.log("False")
+// Switch case 
+    // switch(4){
+    //     case 1:
+    //         console.log("Hello 1")
+    //         break;
+    //     case 2:
+    //         console.log("Hello 2")
+    //         break;
+    //     case 3:
+    //         console.log("Hello 3")
+    //         break;
+    //     default:
+    //         console.log("Default case")
+    // }
+// #####Loops
+// - There are two type of loop -> . straight forward loop (value will be same) . dynamic loop(value will change)
+    // for(startTransition;end;condition)
+    for(var i=1;i<6;i++){
+        console.log("Laptop")
+    }
