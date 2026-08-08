b76aa13deed3bc336101015b793665f9f265730d
Shavej-Malik <shavejsbi@gmail.com>
2026-08-08 12:01:27 +0530

    Loops and Consditional

---
+++ Day-33 Loops And Conditional/script.js
@@ -0,0 +1,62 @@
+//Print 1 to 10
+    // for(let i=1;i<11;i++){
+    //     console.log(i)
+    // }
+//print even number 1 to 20
+    // for(let i=1;i<=20;i++){
+    //     if(i%2===0){
+    //         console.log(i)
+    //     }
+    // }
+//print number 10 to 1 reverse
+    // for(let i=10;i>0;i--){
+    //     console.log(i)
+    // }
+//print "Yes 5 time using looop"
+    // for(let i=1;i<=5;i++){
+    //     console.log("Yes")
+    // }
+//print number 1 to 10 each number check even -> "Even" else "False"
+    // for(let i=1;i<=10;i++){
+    //     if(i%2===0) console.log(`${i} - Even`)
+    //         else console.log(`${i} - Odd`)
+    // }
+//ask number from user and tell positive or negative 
+    // let number = prompt("Enter Your Number")//Prompt always give a string. we have to convert into number
+    //num = Number(number)//Change string into number
+    //1.we can number of any string using "parseInt("12")"
+    //2.we can number of any string using "Number("12")"
+    //3.we can number of any string using "+prompt("anything")"
+    // if(number>0) console.log(`${number} - Positive`)
+    //     else console.log(`${number} - Negative`)
+//when we are giving input from user through the "ptompt".If use press "OK" directly or press "cancel" aur enter only "spaces" Then.......
+
+    // let age = prompt("enter your age?")
+    // if(age===null){
+    //     console.error("Cancelled")
+    // }
+    // else{
+    //     if(/* age.trim() === ""*/ !age){
+    //         console.error("Please write Anything")
+    //     }
+    //     else{
+    //         age = Number(age.trim())
+    //         //NaN===NaN -> false           isNaN(NaN) -> True
+    //         if(isNaN(age)){
+    //             console.error("Please Enter Number")
+    //         }
+    //         else{
+    //             console.log(age)
+    //         } 
+    //     }
+    // } 
+         
+        
+    //if we have spaces forward and backword not middle space of word 
+    //[e.g -> (     Malik    )]Then use ".trim()"
+    // console.log(("         Malik       ").trim())
+//Ask user's age and check for voting 
+    // let age = prompt("Enter your Age ?")
+    // age = Number(age)
+    // if(age>=18) console.log(`${age} Eligible for voting`)
+    //     else console.log(`${age} Not Eligible for voting`)
