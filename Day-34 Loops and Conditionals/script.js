6ef2e0ee7af025b55d4da51ba65ecfb51e0bd180
Shavej-Malik <shavejsbi@gmail.com>
2026-08-09 09:07:03 +0530

    Loops and conditionals

---
+++ Day-34 Loops and Conditionals/script.js
@@ -0,0 +1,57 @@
+//print multiplication table of 5
+    // for(let i=1;i<=10;i++){
+    //     console.log(`6 * ${i} = ${6*i}`)
+    // }
+//count how many number between 1 to 15 are greater than 8
+    // let count=0
+    // for(let i=1;i<=15;i++){
+    //     if(i>8){
+    //         count++
+    //     }
+    // }
+    // console.log(count)
+//ask user password and print access status compare with user input
+    // let password = "Malik"
+    // var input = prompt("Enter your Password")
+    // if(input===null){
+    //     console.error("Cancelled")
+    // }
+    // else{
+    //     if(input===password){
+    //         console.log("Log-in Successfully")
+    //     }
+    //     else{
+    //         console.error("Please Enter Correct Password")
+    //     }
+    // }
+//Allow only 3 attempt to enter password.If user gets it right early stop. if not Account->Locked
+
+//while loop -> 
+    // start                    let i=1
+    // while(ened)              while(i<=10){
+    //     ...........              console.log(i)
+    // updation                     i++
+    //                           }
+
+    // let attempt = 0
+    // let flag = false
+    // let password = "Malik"
+    // let input = prompt("Enter your Password")
+    // attempt++
+    // if(input === password){
+    //     flag = true
+    // }
+    // while(input !== password){
+    //     if(attempt === 3){
+    //         console.error("Account Locked")
+    //         break
+    //     }
+    //     input = prompt("Enter your Password")
+    //     if(input === password){
+    //         flag = true
+    //     }
+    //     attempt++
+    // }
+    // if(flag === true){
+    //     console.log("Log-in Successfully")
+    // }
