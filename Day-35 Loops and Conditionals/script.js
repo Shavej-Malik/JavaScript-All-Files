d6b4d7987d9da7fcc7c8904d36dc1844ce230aa2
Shavej-Malik <shavejsbi@gmail.com>
2026-08-10 17:51:41 +0530

    Loops and Conditionals

---
+++ Day-35 Loops and Conditionals/script.js
@@ -0,0 +1,49 @@
+//Ask words from user until they type "stop".Count how many they typed "Yes"
+    // let user = prompt("Enter your words")
+    // let count = 0
+    // while(user !== "stop"){
+    //     if(user === "yes"){
+    //         count++
+    //     }
+    //     user = prompt("Enter your words")
+        
+    // }
+    // console.log(`Typed "yes" - ${count}`)
+//print number ewhich are divisible by 7 from 1to 50
+    // for(let i=1;i<=50;i++){
+    //     if(i%7===0){
+    //         console.log(i)
+    //     }
+    // }
+//print sum of all odd number from 1 to 30
+    // let sum =0 
+    // for(let i=1;i<=30;i++){
+    //     if(i%2!==0){
+    //         sum=sum+i
+    //     }
+    // }
+    // console.log(`final sum is -> ${sum}`)
+//keep asking number from user until enter even number
+    // let num = +prompt("Enter your Number")
+    // while(num%2!==0){
+    //     num = +prompt("Enter your Number")
+    // }
+//print number between two user input. inout start and end using prompt() -> print all between number
+    // let start = +prompt("Enter Start number")
+    // let end = +prompt("Enter End number")
+    // if(start>end){
+    //     console.error("Please enter start value small, than end")
+    // }
+    // for(let i=start+1;i<=end-1;i++){
+    //     console.log(i);
+    // }
+//print only first 3 odd number between 1 to 20.After 3 odd number,stop
+    // let count = 0
+    // for(let i=1;i<=20;i++){
+    //     if(count===3) break
+    //     if(i%2!==0){
+    //         console.log(i)
+    //         count++
+    //     }
+    // }
+    // console.log(count)
